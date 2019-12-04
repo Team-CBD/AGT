b9a3bfaed49056cfd8160441dfc3dd8ab81a76dc
TwoPheat <twopheat@gmail.com>
2019-12-03 21:15:52 -0800

    Added RunDown Script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://therundown-therundown-v1.p.rapidapi.com/sports/2/events?include=all_periods&include=scores%252Cstatus",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "therundown-therundown-v1.p.rapidapi.com",
+		"x-rapidapi-key": "e0ade11d95mshb80e77a3dfc354cp1c1a92jsn4cc6da73dcc7"
+	}
+}
