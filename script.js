0efdc8e101099eac47a2a2015a3e9153fddb69f5
TwoPheat <twopheat@gmail.com>
2019-12-03 21:23:33 -0800

    included ajax call and console log

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://therundown-therundown-v1.p.rapidapi.com/sports/2/events?include=all_periods&include=scores%252Cstatus",
+	"url": "https://therundown-therundown-v1.p.rapidapi.com/sports/2/openers/2019-06-17?include=scores&include=all_periods",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "therundown-therundown-v1.p.rapidapi.com",
 		"x-rapidapi-key": "e0ade11d95mshb80e77a3dfc354cp1c1a92jsn4cc6da73dcc7"
 	}
 }
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
