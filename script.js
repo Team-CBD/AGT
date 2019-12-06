56d1a481d7c2adb6baedca1f3a2b09b959693b19
Michelle Park <parkm37@yahoo.com>
2019-12-05 20:05:04 -0800

    tested out api

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
+$(document).ready(function(){
+	console.log("aaaaaaaa");
+})
+var date = "2019-12-09";
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://therundown-therundown-v1.p.rapidapi.com/sports/2/openers/2019-06-17?include=scores&include=all_periods",
+	"url": "https://therundown-therundown-v1.p.rapidapi.com/sports/2/events/" + date,
 	"method": "GET",
+	"cnt": "15",
 	"headers": {
 		"x-rapidapi-host": "therundown-therundown-v1.p.rapidapi.com",
 		"x-rapidapi-key": "e0ade11d95mshb80e77a3dfc354cp1c1a92jsn4cc6da73dcc7"
 	}
 }
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
 
+
+function showTeams()
+{
+	$.ajax(settings).done(function (response) {
+		$.each(response.events, function(index, val){
+			console.log(val.teams[0].name);
+		})
+	});
+}
+
+showTeams();
 
 
 
 //---------Materialize Controls--------------------------
 
-<TopAppBar
+/*<TopAppBar>
 title='Any Given Team'
 short
 navigationIcon={<MaterialIcon
@@ -30,4 +43,4 @@ actionItems={[
   <MaterialIcon icon='print' />,
   <MaterialIcon icon='bookmark' />,
 ]}
-/>
+</TopAppBar>*/
