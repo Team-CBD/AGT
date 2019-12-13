8b7c08ab08edb6bb9fd5474df135996286cf6ece
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2019-12-13 10:50:23 -0800

    debugged: logos appear, teams populate

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,46 @@ var date = new Date(); //full year month date
 var day = String(date.getDate()).padStart(2, '0');
 var month = String(date.getMonth() + 1).padStart(2, '0');
 var year = date.getFullYear();
-var today = year + "-" + month + "-" + day;
+var today = "2019-12-08"; //year + "-" + month + "-" + day;
 console.log(today);
 var gameId = "";
+var rundownAPIkey = "afbd06620amshd8f0c0962c6e84bp1e8773jsn7e85a0219911"; //<=#4 //"a0f94d59d3msh5288a8373770889p1430d2jsn2fb820f83154"; // <= deri's 3rd key
+
+var theRundownData = [];
 
 var settings = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://therundown-therundown-v1.p.rapidapi.com/sports/2/events/" + today,
 	"method": "GET",
+	"dataType": "json",
 	"headers": {
 		"x-rapidapi-host": "therundown-therundown-v1.p.rapidapi.com",
-		"x-rapidapi-key": "84cbe2eb38msh070a5cb084d0089p1caeebjsn7e60c285acd6"
+		"x-rapidapi-key": rundownAPIkey
 	}
 }
+
+$.ajax(settings).done(function(data){
+	theRundownData.push(data);
+	console.log(data);
+	console.log(theRundownData[0]);
+});
+
 
 function gameIdGet(event)
 {
 	var gameId = event.target.id;
-	console.log(gameId);
-	$.ajax(settings).done(function(response)
-	{
-		var index = response.events.findIndex(x => x.event_id === gameId);
-		console.log(index);
+	console.log("gameId: " + gameId);
+	
+		var index = theRundownData[0].events.findIndex(x => x.event_id === gameId);
+		console.log("Index: " + index);
 		showTeams(index);
 		getScores(gameId);
-		$("#drop-fill").hide(5);
-		$(".team-banner").show(500);
-	});
+		
+		
+		//$(".drop-fill").hide(5);
+		//$(".team-banner").show(500);
+	
 }
 //TODO: function that does all the logos and pastes the team scores up
 
@@ -43,7 +55,7 @@ function getScores(gameId)
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-host": "therundown-therundown-v1.p.rapidapi.com",
-			"x-rapidapi-key": "84cbe2eb38msh070a5cb084d0089p1caeebjsn7e60c285acd6"
+			"x-rapidapi-key": rundownAPIkey
 		}
 	}
 	
@@ -99,25 +111,41 @@ function getScores(gameId)
 
 function showTeams(index)
 {
-	$.ajax(settings).done(function(response)
+	$("#dropFill").hide();
+	$("#team-banner").show();
+	console.log("gameTime clicked");
+
+	var logoLeftDiv = $("#team-logo-left");
+	var logoRightDiv = $("#team-logo-right");
+
+	var imgSrc = "assets/img/";
+	var logoLeft = theRundownData[0].events[index].teams_normalized[0].mascot.toLowerCase();
+	var logoRight = theRundownData[0].events[index].teams_normalized[1].mascot.toLowerCase();
+
+	console.log(theRundownData[0].events[index].teams_normalized[0].mascot.toLowerCase());
+	console.log("src", imgSrc + logoLeft + ".gif");
+
+	$("<img>").attr("src", imgSrc + logoLeft + ".gif").attr("alt", logoLeft + "Logo").prependTo(logoLeftDiv);
+	$("<img>").attr("src", imgSrc + logoRight + ".gif").attr("alt", logoRight + "Logo").prependTo(logoRightDiv);
+
+	document.body.innerHTML = document.body.innerHTML.replace("team name 1", theRundownData[0].events[index].teams[0].name);
+	document.body.innerHTML = document.body.innerHTML.replace("team name 2", theRundownData[0].events[index].teams[1].name);
+	
+	console.log("Are the " + theRundownData[0].events[index].teams[0].name + " at home? - " + theRundownData[0].events[index].teams[0].is_home);
+	
+	if(theRundownData[0].events[index].teams[0].is_home === true)
 	{
-		document.body.innerHTML = document.body.innerHTML.replace("team name 1", response.events[index].teams[0].name);
-		document.body.innerHTML = document.body.innerHTML.replace("team name 2", response.events[index].teams[1].name);
-		console.log(response);
-		console.log(response.events[index].teams[0].is_home);
-		if(response.events[index].teams[0].is_home === true)
-		{
-			document.body.innerHTML = document.body.innerHTML.replace("Info Left", "Home");
-			document.body.innerHTML = document.body.innerHTML.replace("Info Right", "Away");
-			getLatLon(response.events[index].teams_normalized[0].name, index);
-		}
-		else
-		{
-			document.body.innerHTML = document.body.innerHTML.replace("Info Left", "Away");
-			document.body.innerHTML = document.body.innerHTML.replace("Info Right", "Home");
-			getLatLon(response.events[index].teams_normalized[1].name, index);
-		}
-	})
+		document.body.innerHTML = document.body.innerHTML.replace("Info Left", "Home");
+		document.body.innerHTML = document.body.innerHTML.replace("Info Right", "Away");
+		getLatLon(theRundownData[0].events[index].teams_normalized[0].name, index);
+	}
+	else
+	{
+		document.body.innerHTML = document.body.innerHTML.replace("Info Left", "Away");
+		document.body.innerHTML = document.body.innerHTML.replace("Info Right", "Home");
+		getLatLon(theRundownData[0].events[index].teams_normalized[1].name, index);
+	}
+	
 	
 }
 
@@ -143,11 +171,10 @@ function getLatLon(city, index)
 function showWeather(latitude, longitude, index)
 {
 	var key = "cdd78f42904565ed23569354e5f2ea6c";
-	$.ajax(settings).done(function(response)
-	{
-		var time = response.events[index].event_date;
+	
+		var time = theRundownData[0].events[index].event_date;
 		var unixTime = moment(time).unix();
-	})
+	
 	$.ajax(
 		{
 			crossDomain: true,
@@ -158,50 +185,106 @@ function showWeather(latitude, longitude, index)
 		}).then(function(response)
 		{
 			var temperature = response.currently.apparentTemperature;
-			console.log(temperature); //TODO: display current weather on page
+			console.log("temp: " + temperature); //TODO: display current weather on page
 		});
 }
 
-/*function fillDropDown()
-{
-	var dropItems = "";
-	$.ajax(settings).done(function(response)
-	{
-		$.each(response.events, function(index,val)
-		{
-			dropItems += "<br>";
-			dropItems += "<br><a class='dropdown-item btn gameTime flow-text grey lighten-4 z-depth-1 black-text' onclick='gameIdGet(event)' id=" + response.events[index].event_id + ">" + val.teams[0].name + " vs. " + val.teams[1].name + "</div>";
-			dropItems += "<br>"; 
-		});
-		$("#dropFill").html(dropItems);
-		//console.log(response);
-	});
-}*/
 
 function fillDropDown()
 {
-	var dropItems = "";
-	$.ajax({
-		url: "https://api.openweathermap.org/data/2.5/forecast",
-		method: "GET",
-		data:{
-			q: "London",
-			appid: "166a433c57516f51dfab1f7edaed8413",
-			units: "imperial"
-		}
-	}).then(function(response){
-		console.log(response);
-		$.each(response.list, function(index){
-			dropItems += "<br>";
-			dropItems += "<button class='dropdown-item gameTime btn flow-text grey lighten-4 z-depth-1 black-text' onclick='gameIdGet(event)' id=" + response.list[index].main.temp + ">" + response.list[index].main.temp + "This is some placeholder ass shit, leave it be, thanks, mgmt.</button>";
-			dropItems += "<br>";
-		});
-		
-		$("#dropFill").html(dropItems);
+var dropItems = "";
+
+	$.each(theRundownData[0].events, function(index,val)
+	{
+		dropItems += "<br>";
+		dropItems += "<br><a class='dropdown-item btn gameTime flow-text grey lighten-4 z-depth-1 black-text' onclick='gameIdGet(event)' id=" + theRundownData[0].events[index].event_id + ">" + val.teams[0].name + " vs. " + val.teams[1].name + "</div>";
+		dropItems += "<br>";
 	});
+	$("#dropFill").html(dropItems);
+	
 }
 
-subMit.addEventListener("click", function()
-{
-	fillDropDown();
+
+paypal.Buttons({
+	style: {
+		shape: 'pill',
+		color: 'gold',
+		layout: 'vertical',
+		label: 'pay',
+		
+	},
+	createOrder: function(data, actions) {
+		return actions.order.create({
+			purchase_units: [{
+				amount: {
+					value: '1'
+				}
+			}]
+		});
+	},
+	onApprove: function(data, actions) {
+		return actions.order.capture().then(function(details) {
+			alert('Transaction completed by ' + details.payer.name.given_name + '!');
+		});
+	}
+}).render('#paypal-button-container');
+
+
+    //<!--  Below is the transition flow for the frontend. When one element is used, it hides and brings in the next-->
+ 
+
+	//---Hide n Seek------
+$(document).ready(function(){
+	$("#subMit").click(function(e) 
+	{
+		event.preventDefault();
+		// Get input name
+		var eMail = $("#email_inline").val();
+			// Store data
+			localStorage.setItem("email_inline", eMail);
+			document.getElementById("email-output1").innerHTML = localStorage.getItem("email_inline");
+			console.log("Your e-mail is saved:" + eMail);
+			
+			$("#email-area").hide();
+			$(".drop-fill").show()
+			fillDropDown();
+	});
+
+	$("#paypal-button-container").hide();
+	$(".tShirts").hide();
+	$(".drop-fill").hide();
+	$("#team-banner").hide();
+
+	// $(".gameTime").click(function(e)
+	// {
+	// 	//event.preventDefault();
+	// 	$(".drop-fill").hide();
+	// 	$("#team-banner").show();
+	// 	console.log("gameTime clicked");
+	// });
+
+	$("#left-team").click(function(e)
+	{
+		//event.preventDefault();
+		$("#team-banner").hide();
+		$(".tShirts").show();
+		$("#leftT-shirt").hide();
+		$("#rightT-shirt").show();
+		$("#paypal-button-container").show();
+		console.log("left-team clicked");
+	});
+	
+	$("#right-team").click(function(e)
+	{
+	//event.preventDefault();
+	$("#team-banner").hide();
+	$(".tShirts").show();
+	$("#rightT-shirt").hide();
+	$("#leftT-shirt").show();
+	$("#paypal-button-container").show();
+	console.log("right-team clicked");
+	});
+	
+
 });
+    
